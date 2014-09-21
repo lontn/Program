824ab19acb7db5fa6846a8b8eb69e6b0097272cb
lontn <lontn@hotmail.com>
2014-09-21 12:49:23 +0800

    + new crawler project

---
+++ JAVA/crawler-fcu/src/main/java/org/fcu/green/crawler/CrawlerRun.java
@@ -0,0 +1,50 @@
+package org.fcu.green.crawler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class CrawlerRun {
+    private static final Logger L = LoggerFactory.getLogger(CrawlerRun.class);
+    public static void main(String[] args) {
+        String crawlStorageFolder = "D:/auto_pricing/FcuCrawler";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = null;
+        try {
+            controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        } catch (Exception e) {
+            L.debug("CrawlerRun Error.", e);
+        }
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("http://open.umn.edu/opentextbooks/SearchResults.aspx?subjectAreaId=1");
+//        controller.addSeed("http://www.ics.uci.edu/~lopes/");
+//        controller.addSeed("http://www.ics.uci.edu/");
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(MyCrawler.class, numberOfCrawlers);
+}
+
+}
---
+++ JAVA/crawler-fcu/src/main/java/org/fcu/green/crawler/MyCrawler.java
@@ -0,0 +1,55 @@
+package org.fcu.green.crawler;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class MyCrawler extends WebCrawler {
+    private static final Logger L = LoggerFactory.getLogger(MyCrawler.class);
+
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" 
+                                                      + "|png|tiff?|mid|mp2|mp3|mp4"
+                                                      + "|wav|avi|mov|mpeg|ram|m4v|pdf" 
+                                                      + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+
+    /**
+     * You should implement this function to specify whether
+     * the given url should be crawled or not (based on your
+     * crawling logic).
+     */
+    @Override
+    public boolean shouldVisit(WebURL url) {
+            String href = url.getURL().toLowerCase();
+            return !FILTERS.matcher(href).matches() && href.startsWith("http://open.umn.edu/opentextbooks/");
+    }
+
+    /**
+     * This function is called when a page is fetched and ready 
+     * to be processed by your program.
+     */
+    @Override
+    public void visit(Page page) {          
+            String url = page.getWebURL().getURL();
+            System.out.println("URL: " + url);
+
+            if (page.getParseData() instanceof HtmlParseData) {
+                    HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+                    String text = htmlParseData.getText();
+                    String html = htmlParseData.getHtml();
+                    List<WebURL> links = htmlParseData.getOutgoingUrls();
+                    
+                    L.info("Text length: " + text.length());
+                    L.info("text Template:{}", text);
+                    L.info("Html length: " + html.length());
+                    L.info("html Template:{}", html);
+                    L.info("Number of outgoing links: " + links.size());
+            }
+    }
+}
