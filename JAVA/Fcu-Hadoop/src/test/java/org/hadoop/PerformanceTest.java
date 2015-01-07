f7d00ca185b973ecad9efd302c718d1527cd9704
freelon <lontn.chang@gmail.com>
2015-01-07 23:08:24 +0800

    + add JUnit

--- JAVA/Fcu-Hadoop/src/test/java/org/hadoop/PerformanceTest.java
+++ JAVA/Fcu-Hadoop/src/test/java/org/hadoop/PerformanceTest.java
@@ -1,8 +1,17 @@
 package org.hadoop;
 
+import java.io.IOException;
+import java.net.URL;
+
+//import org.codehaus.jackson.JsonParser;
+//import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PerformanceTest {
     private static final Logger L = LoggerFactory.getLogger(PerformanceTest.class);
@@ -18,5 +27,22 @@ public class PerformanceTest {
         String freeMemory = String.format("%,d M", runtime.freeMemory() / 1000000);
 
         L.info("Memory: [Max : " + maxMemory + "]" + " [Total : " + totalMemory + "]" + " [Free : " + freeMemory + "]");
+    }
+
+    @Test
+    public void JsonTest() {
+        String url = "http://sid.geo.com.tw/FUCGoogleMapWebAPI/api/FCU/Get";
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); //ignore 未知的屬性
+        try {
+            FcuFood fcu = mapper.readValue(new URL(url), FcuFood.class);
+            System.out.println(fcu);
+            System.out.println("Name:"+fcu.getName());
+            System.out.println("Address:"+fcu.getAddress());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
