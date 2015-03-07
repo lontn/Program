a03748d95c8a929d7833446f0148a7febfe6a291
lontn <lontn@hotmail.com>
2015-03-07 20:42:49 +0800

    +add Python Project

---
+++ Python/PythonTest/src/Operators3Test.py
@@ -0,0 +1,19 @@
+flag = False;
+name = "Lon"
+if(name == "Kobe"):
+    flag = True;
+    print('welcome boss');
+else:
+    print(name);
+
+num = 5
+if(num == 3):
+    print("boss");
+elif(num == 2):
+    print("user");
+elif(num == 1):
+    print("worker");
+elif(num < 0):
+    print("error");
+else:
+    print("roadman");
---
+++ Python/PythonTest/src/pydevTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#    -*- coding: utf-8 -*-
+"""
+Function:
+【记录】使用Python的IDE：Eclipse+PyDev
+
+http://www.crifan.com/try_with_python_ide_eclipse_pydev
+
+Author:     Crifan Li
+Version:    2012-12-29
+Contact:    admin at crifan dot com
+"""
+
+import platform;
+from pip._vendor.distlib.compat import raw_input
+
+print("Current platform.uname() in Ecplise+PyDev=", platform.uname());
+
+raw_input("\n\nPress the enter key to exit.");
